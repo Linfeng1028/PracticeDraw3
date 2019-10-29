55ffa8b5e7c67275258d5201de060be2d4400e49
linfeng.wang <linfeng.wang@juphoon.com.cn>
2019-10-29 15:52:00 +0800

    feature: 完成练习项目

--- app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice13GetTextBoundsView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw3/practice/Practice13GetTextBoundsView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -48,13 +49,32 @@ public class Practice13GetTextBoundsView extends View {
         // 使用 Paint.getTextBounds() 计算出文字的显示区域
         // 然后计算出文字的绘制位置，从而让文字上下居中
         // 这种居中算法的优点是，可以让文字精准地居中，分毫不差
-
+        Rect bounds = new Rect();
+        int height = 0;
         int middle = (top + bottom) / 2;
-        canvas.drawText(text1, 100, middle, paint2);
-        canvas.drawText(text2, 200, middle, paint2);
-        canvas.drawText(text3, 300, middle, paint2);
-        canvas.drawText(text4, 400, middle, paint2);
-        canvas.drawText(text5, 500, middle, paint2);
-        canvas.drawText(text6, 600, middle, paint2);
+
+        paint2.getTextBounds(text1, 0, 1, bounds);
+        height = -(bounds.bottom + bounds.top) / 2;
+        canvas.drawText(text1, 100, middle + height, paint2);
+
+        paint2.getTextBounds(text2, 0, 1, bounds);
+        height = -(bounds.bottom + bounds.top) / 2;
+        canvas.drawText(text2, 200, middle +height, paint2);
+
+        paint2.getTextBounds(text3, 0, 1, bounds);
+        height = -(bounds.bottom + bounds.top) / 2;
+        canvas.drawText(text3, 300, middle +height, paint2);
+
+        paint2.getTextBounds(text4, 0, 1, bounds);
+        height = -(bounds.bottom + bounds.top) / 2;
+        canvas.drawText(text4, 400, middle +height, paint2);
+
+        paint2.getTextBounds(text5, 0, 1, bounds);
+        height = -(bounds.bottom + bounds.top) / 2;
+        canvas.drawText(text5, 500, middle +height, paint2);
+
+        paint2.getTextBounds(text6, 0, 1, bounds);
+        height = -(bounds.bottom + bounds.top) / 2;
+        canvas.drawText(text6, 600, middle +height, paint2);
     }
 }
